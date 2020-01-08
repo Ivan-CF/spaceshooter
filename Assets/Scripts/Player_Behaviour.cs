40f8fc18e9911ff67309c9b9db09780c879d1b83
IvanCF <ivanncafe@gmail.com>
2020-01-08 12:11:23 +0100

    Nave se mueve

---
+++ Assets/Scripts/Player_Behaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Behaviour : MonoBehaviour
+{
+    public float speed;
+    private Vector2 axis;
+
+    void Update () {
+        transform.Translate(axis * speed * Time.deltaTime);
+    }
+    public void SetAxis (Vector2 currentaxis){
+        axis = currentaxis;
+    }
+    
+}
---
+++ Assets/Scripts/Ship_Input.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Input : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2 axis;
+
+    public Player_Behaviour player;  
+
+    // Update is called once per frame
+    void Update()
+    {
+        axis.x = Input.GetAxis ("Horizontal");
+        axis.y = Input.GetAxis ("Vertical");
+
+        //Debug.Log("x:"+axis.x+" y:"+axis.y);
+        player.SetAxis(axis);
+    }
+}
