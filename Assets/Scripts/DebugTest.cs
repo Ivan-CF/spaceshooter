8093b218e8810969e3ea9fef2e6b9b9d53827e66
IvanCF <ivanncafe@gmail.com>
2020-01-08 10:06:49 +0100

    first commit

---
+++ Assets/Scripts/DebugTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTest : MonoBehaviour
+{
+    //Propiedad
+    public string debugString;
+
+    //Campo
+    public int numeroRuedas { get; set; }
+
+    void Awake(){
+        debugString = "Hola como estas";
+
+        Debug.Log(debugString);
+
+        numeroRuedas = 3;
+        
+        Debug.Log(numeroRuedas);
+
+    }
+}
