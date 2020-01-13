0d7c2f57c5f2f653bd66553293188d011ce795fb
IvanCF <ivanncafe@gmail.com>
2020-01-13 10:04:52 +0100

    Con limites y Background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Background : MonoBehaviour {
+ 
+    public Material mat;
+    public float smooth = 1.0f;
+    private float offsetX = 0.0f;
+ 
+    private Vector2 textoffset;
+ 
+ 
+ 
+    // Use this for initialization
+    void Awake () {
+        mat = GetComponent<Renderer> ().material;
+        textoffset = new Vector2 (offsetX,0);
+    }
+   
+    // Update is called once per frame
+    void Update () {
+        offsetX += (Time.deltaTime * (smooth  + 75/100));
+        if(offsetX >= 100) offsetX -= 100;
+ 
+        textoffset.x = offsetX;
+        mat.SetTextureOffset("_MainTex", textoffset);
+    }
+ 
+}
--- Assets/Scripts/Player_Behaviour.cs
+++ Assets/Scripts/Player_Behaviour.cs
@@ -6,12 +6,31 @@ public class Player_Behaviour : MonoBehaviour
 {
     public float speed;
     private Vector2 axis;
+    public Vector2 limits;
 
     void Update () {
-        transform.Translate(axis * speed * Time.deltaTime);
+        transform.Translate (axis * speed * Time.deltaTime);
+ 
+        if (transform.position.x > limits.x) {
+            transform.position = new Vector3 (limits.x, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -limits.x) {
+            transform.position = new Vector3 (-limits.x, transform.position.y, transform.position.z);
+        }
+ 
+        if (transform.position.y > limits.y) {
+            transform.position = new Vector3 (transform.position.x, limits.y, transform.position.z);
+        }
+        else if (transform.position.y < -limits.y) {
+            transform.position = new Vector3 (transform.position.x, -limits.y, transform.position.z);
+        }
     }
-    public void SetAxis (Vector2 currentaxis){
-        axis = currentaxis;
+    public void SetAxis (Vector2 currentAxis){
+        axis = currentAxis;
     }
+    public void SetAxis(float x, float y){
+        axis= new Vector2(x,y);
+	}
+
     
 }
