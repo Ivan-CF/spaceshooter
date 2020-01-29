2d1eaaaa674fcae902708a276e2b7fb72b17ec56
IvanCF <ivanncafe@gmail.com>
2020-01-29 12:10:09 +0100

    Enemies

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private Vector2 speed = new Vector2(0, 0);
+    public GameObject Bullet;
+
+    private void Start()
+    {
+        StartCoroutine(EnemyBehaviour());
+    }
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime);
+    }
+
+    IEnumerator EnemyBehaviour()
+    {
+        while (true)
+        {
+            //Avanza horizontalmente
+            
+            speed.x = -1f;
+            speed.y = 1f;
+            yield return new WaitForSeconds(1.0f);
+
+            //Se para
+            speed.x = 0f;
+            speed.y = 0f;
+            yield return new WaitForSeconds(1.0f);
+
+            speed.x =-1f;
+            speed.y = -1f;
+            yield return new WaitForSeconds(1.0f);
+
+            speed.x = 0f;
+            speed.y = 0f;
+            yield return new WaitForSeconds(1.0f);
+
+            //Dispara
+            GameObject bala = Instantiate(Bullet, transform.position, Quaternion.identity, null);
+            bala.transform.Rotate(0, 0, -180);
+        }
+    }
+}
