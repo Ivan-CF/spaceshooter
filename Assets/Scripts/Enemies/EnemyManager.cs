1deae75cdff603b1606c50e18b1fe5ce6690af6b
IvanCF <ivanncafe@gmail.com>
2020-02-03 09:55:38 +0100

    Main Menu

---
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject[] formaciones;
+    public float timeLaunchFormation;
+ 
+    private float currentTime = 0;
+ 
+    void Awake(){
+        StartCoroutine(LanzaFormacion());
+    }
+ 
+    IEnumerator LanzaFormacion(){
+        int formacionActual=0;
+        while(true){
+            formacionActual = Random.Range(0,formaciones.Length);
+ 
+            Instantiate(formaciones[formacionActual],new Vector3(10,Random.Range(-5,5)),Quaternion.identity,this.transform);
+            yield return new WaitForSeconds(timeLaunchFormation);
+        }
+    }
+ 
+}
---
+++ Assets/Scripts/Meteors/MeteorManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorManager : MonoBehaviour
+{
+    public GameObject meteorPrefab;
+    public float timeLauchMeteor;
+
+    private float currentTime = 0;
+
+    void Awake(){
+        StartCoroutine(LanzaMeteoritos());
+    }
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime>timeLauchMeteor){
+            currentTime = 0;
+            Instantiate(meteorPrefab,new Vector3(10,Random.Range(-5,5)),Quaternion.identity,this.transform);
+        }
+    }
+    
+
+    IEnumerator LanzaMeteoritos(){
+        while(true){
+            Instantiate(meteorPrefab,new Vector3(10,Random.Range(-5,5)),Quaternion.identity,this.transform);
+            yield return new WaitForSeconds(timeLauchMeteor);
+        }
+    }
+
+}
